8be197c97916d7b94991f8ab1112f24d2001f800
rem <tien23851@gmail.com>
2024-12-13 23:20:32 +0700

    .

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,6 @@ async function bootstrap() {
     preflightContinue: false,
     credentials: true,
   })
-  await app.listen(configService.get("PORT") || 4000);
+  await app.listen(4000);
 }
 bootstrap();
